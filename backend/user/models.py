cbe9c23bac85a5d3f0c3a400206e8247d0dc2f3d
takanda <solvency81@gmail.com>
2024-01-09 20:34:36 +0900

    Backend setup

---
+++ backend/user/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, password, **extra_fields):
+        if not email:
+            raise ValueError("Email is required.")
+        if not username:
+            raise ValueError("Username is required.")
+        if not password:
+            raise ValueError("Password is required.")
+
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.is_active = True
+        user.save()
+        return user
+    
+    def create_superuser(self, email, username, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_staff", True)
+
+        return self.create_user(email, username, password, **extra_fields)
+
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    user_id = models.AutoField(primary_key=True)
+    email = models.EmailField(unique=True, max_length=256)
+    username = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    objects = UserManager()
+
+    class Meta:
+        db_table = "UserModel"
+
+    def __str__(self):
+        return self.username
